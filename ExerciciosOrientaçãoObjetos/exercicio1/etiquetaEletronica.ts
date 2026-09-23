36107beda333b0a881b4e99a3dec7425c801ea2c
FELIPE SANTOS IENSEN <felipeiensen@alunos.utfpr.edu.br>
2026-09-23 20:41:59 -0300

    Add OOP exercise and rename files
    Adds the new electronic label exercise scaffold under ExerciciosOrientacaoObjetos/exercicio1 and initializes its index file. Also renames the math study note and velocity media file to clearer, corrected names.

---
+++ ExerciciosOrientaçãoObjetos/exercicio1/etiquetaEletronica.ts
@@ -0,0 +1,15 @@
+export default class etiquetaEletronica {
+    private pesoQuilo: number;
+}
+
+public constructor(pesoQuilo: number) {
+    this.pesoQuilo = pesoQuilo;
+}
+
+public getPesoQuilo(): number {
+    return this.peso;
+}
+
+public setPesoQuilo(pesoQuilo: number): void {
+    this.pesoQuilo = pesoQuilo;
+}
